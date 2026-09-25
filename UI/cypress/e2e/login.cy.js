38195485417811978c15e6f2bb96c8cbe06c7c9a
Pedro Ricardo <pedro-ricardo.belo@hotmail.com>
2026-09-25 09:42:47 -0300

    ajustando login.test

--- UI/cypress/e2e/login.cy.js
+++ UI/cypress/e2e/login.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+
 let dadosLogin;
 
 describe("US-0002 - Login na plataforma", () => {
@@ -14,22 +15,52 @@ describe("US-0002 - Login na plataforma", () => {
 
   it("CT-005 - Deve realizar login com credenciais válidas", () => {
     cy.login(dadosLogin.usuario, dadosLogin.senha);
+
     cy.get(".page-title").should("exist");
   });
 
-  it("CT-005 - Deve realizar login utilizando fixture", () => {
-    cy.fixture("perfil").then((dados) => {
-      cy.login(dados.usuario, dados.senha);
+  it("CT-006 - Deve impedir login com senha inválida", () => {
+    cy.login(dadosLogin.usuario, "SenhaInvalida123");
+
+    cy.get("body").then(($body) => {
+      cy.log("===== TEXTO DA PÁGINA =====");
+      cy.log($body.text());
+
+      cy.log("===== ELEMENTOS DE ERRO =====");
+
+      $body.find(
+        ".woocommerce-error, .woocommerce-message, .woocommerce-NoticeGroup, [role='alert']"
+      ).each((index, element) => {
+        cy.log(
+          `ERRO ${index + 1}: ${Cypress.$(element).text().trim()}`
+        );
+      });
     });
-    cy.get(".page-title").should("exist");
   });
 
-  it("CT-005 - Deve realizar login utilizando os elementos diretamente", () => {
-    cy.fixture("perfil").then((dados) => {
-      cy.get("#username").type(dados.usuario);
-      cy.get("#password").type(dados.senha, { log: false });
-      cy.get(".woocommerce-form > .button").click();
-      cy.get(".page-title").should("exist");
+  it("CT-007 - Deve impedir login sem informar o usuário", () => {
+    cy.get("#password").type(dadosLogin.senha, { log: false });
+
+    cy.get("#username").then(($input) => {
+      cy.log(`required: ${$input.prop("required")}`);
+      cy.log(`value: "${$input.val()}"`);
     });
+
+    cy.get(".woocommerce-form > .button").click();
+
+    cy.url().should("include", "minha-conta");
+  });
+
+  it("CT-008 - Deve impedir login sem informar a senha", () => {
+    cy.get("#username").type(dadosLogin.usuario);
+
+    cy.get("#password").then(($input) => {
+      cy.log(`required: ${$input.prop("required")}`);
+      cy.log(`value: "${$input.val()}"`);
+    });
+
+    cy.get(".woocommerce-form > .button").click();
+
+    cy.url().should("include", "minha-conta");
   });
 });
